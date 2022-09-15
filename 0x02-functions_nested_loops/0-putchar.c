72c1568d8cb82dde729f1eaa1b55310201598600
Samsegdamien <107221883+Samsegdamien@users.noreply.github.com>
2022-09-15 22:04:02 +0100

    Update 0-putchar.c
    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
-#include <main.h>
-  
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always returns 0 
-  * 
-  */ 
-  
- int main(void) 
- { 
- 	_putchar('_'); 
-	_putchar('p'); 
-	_putchar('u'); 
-	_putchar('t'); 
-	_putchar('c'); 
-	_putchar('h'); 
-	_putchar('a'); 
-	_putchar('r'); 
-	_putchar('\n'); 
-	return (0); 
- }
+#include "main.h"
+/**
+*main - entry point
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+char text[] = "_putchar";
+
+while (text[i] != '\0')
+{
+_putchar(text[i]);
+i++;
+}
+_putchar('\n');
+
+return (0);
+} 
