21a690b336f4c6ad85a740aae7c0415b99084542
Samsegdamien <107221883+Samsegdamien@users.noreply.github.com>
2022-09-16 11:36:59 +0100

    Create 3-islower.c
    Task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character.
+*@c: character to check for lowercase letter
+*
+*Return: (1) if c is lowercase, (0) if not
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+} 
