1acaf10cc7f9e5ee855073154ee5c6b374547bd2
Samsegdamien <107221883+Samsegdamien@users.noreply.github.com>
2022-09-16 11:47:21 +0100

    Create 6-abs.c
    Task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _abs - find absolute value of an integer
+* @num: integer to check
+*
+* Return: absolute value of num
+*/
+
+int _abs(int num)
+{
+if (num < 0)
+{
+return (-num);
+}
+return (num);
+}
