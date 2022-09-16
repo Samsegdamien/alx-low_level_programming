cb0a7a1db916bf9efec27c85afd056e6f4240cc5
samsegdamien <bayoekene10@gmail.com>
2022-09-16 04:35:05 -0700

    file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,37 @@
 #include "main.h" 
-#include "6-abs.c" 
-#include <stdio.h> 
+ #include "6-abs.c" 
+ #include <stdio.h> 
   
-/** 
-* print_to_98 - prints numbers 
-* 
-* @n: integer to be passed 
-* 
-*/ 
+ /** 
+  * print_to_98 - prints numbers 
+  * 
+  * @n: integer to be passed 
+  * 
+  */ 
   
-void print_to_98(int n) 
-{ 
-int i; 
+ void print_to_98(int n) 
+ { 
+	 int i; 
   
-if (n > 98) 
-{ 
-for (i = n; i >= 98; i--) 
-{ 
-printf("%d", i); 
-if (i != 98) 
-{ 
-printf(", "); 
-} 
-} 
-} else 
-{ 
-for (i = n; i <= 98; i++) 
-{ 
-printf("%d", i); 
-if (i != 98) 
-{ 
-printf(", "); 
-} 
-} 
-} 
-printf("\n"); 
+	if (n > 98) 
+	{ 
+		for (i = n; i >= 98; i--) 
+		{ 
+			printf("%d", i); 
+			if (i != 98) 
+			{ 
+				printf(", ");
+			} 
+	} else 
+	{ 
+		for (i = n; i <= 98; i++) 
+		{ 
+			printf("%d", i); 
+			if (i != 98) 
+			{ 
+				printf(", "); 
+			} 
+		} 
+	}
+	printf("\n"); 
 }
