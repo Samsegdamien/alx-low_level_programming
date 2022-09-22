9f0846ff1fba6a568f239ca4469f7affd3e2fb56
samsegdamien <bayoekene10@gmail.com>
2022-09-22 14:38:12 -0700

    filr

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
- /**
- *rev_string - A function that reverse a string
- *@s: The string to be reversed;
- *Return: void
- */
+/**
+*rev_string - A function that reverse a string
+*@s: The string to be reversed;
+*Return: void
+*/
 
- void rev_string(char *s)
- {char *first = s;
- char aplha;
- for (; *(s + 1); )
- {
- s++;
- }
- while (first < s)
- {aplha = *first;
- *first = *s;
- *s = aplha;
- s--;
- first++;
- }
- }
+void rev_string(char *s)
+{char *first = s;
+char aplha;
+for (; *(s + 1); )
+{
+s++;
+}
+while (first < s)
+{aplha = *first;
+*first = *s;
+*s = aplha;
+s--;
+first++;
+}
+}
