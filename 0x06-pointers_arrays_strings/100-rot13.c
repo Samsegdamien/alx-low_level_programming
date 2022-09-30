9eefd1b50e60605fc7acf36762b19d4aa1bbb035
samsegdamien <bayoekene10@gmail.com>
2022-09-30 12:51:30 -0700

    file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,25 @@
-#include "main.h" 
-  
-/** 
-*rot13 - a function that encodes a string using rot13. 
-*@values: strings to encode 
-*Return: Retuns encoded strings 
-*/ 
-  
-char *rot13(char *values) 
-{char *encoder1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
-char *encoder2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"; 
-int i, j; 
-for (i = 0; values[i] != '\0'; i++) 
-{ 
-for (j = 0; encoder1[j] != '\0'; j++) 
-{ 
-if (values[i] == encoder1[j]) 
-{ 
-values[i] = encoder2[j]; 
-break; 
-} 
-} 
+#include "main.h"
+
+/**
+*rot13 - a function that encodes a string using rot13.
+*@values: strings to encode
+*Return: Retuns encoded strings
+*/
+
+char *rot13(char *values)
+{char *encoder1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *encoder2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, j;
+for (i = 0; values[i] != '\0'; i++)
+{
+for (j = 0; encoder1[j] != '\0'; j++)
+{
+if (values[i] == encoder1[j])
+{
+values[i] = encoder2[j];
+break;
 }
-return (values); 
+}
+}
+return (values);
 }
