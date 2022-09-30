83b32703e44c470b88bfc1a7188217e978603abc
samsegdamien <bayoekene10@gmail.com>
2022-09-30 12:09:23 -0700

    fole

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,38 @@
-#include "main.h" 
-  
-/** 
-**cap_string - capitalizes all words of a string. 
-*@s: Place holder for arguement 
-*Return: Returns 
-*/ 
-  
-  
-  
-char *cap_string(char *s) 
-{int i = 0; 
-int j; 
-char a[] = " \t\n,;.!?\"(){}"; 
-while (*(s + i)) 
-{ 
-if (*(s + i) >= 'a' && *(s + i) <= 'z') 
-{ 
-if (i == 0) 
-{ 
-*(s + i) -= 'a' - 'A'; 
-	
-} 
-else 
-{ 
-for (j = 0; j <= 12; j++) 
-{ 
-if (a[j] == *(s + i - 1)) 
-{ 
-*(s + i) -= 'a' - 'A'; 
-} 
-} 
-} 
-} 
-i++; 
-} 
-return (s); 
+#include "main.h"
+
+/**
+**cap_string - capitalizes all words of a string.
+*@s: Place holder for arguement
+*Return: Returns
+*/
+
+
+
+char *cap_string(char *s)
+{int i = 0;
+int j;
+char a[] = " \t\n,;.!?\"(){}";
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+{
+if (i == 0)
+{
+*(s + i) -= 'a' - 'A';
+
+}
+else
+{
+for (j = 0; j <= 12; j++)
+{
+if (a[j] == *(s + i - 1))
+{
+*(s + i) -= 'a' - 'A';
+}
+}
+}
+}
+i++;
+}
+return (s);
 }
